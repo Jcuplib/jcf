2e704f053014e3fe61e642e8f90c678480808b7c
Takahiro INOUE <tinoue@rist.jp>
2014-09-18 11:17:55 +0900

    Added automated test execution script.

---
+++ test/do_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+TESTS="\
+cal_great_circle_area \
+cal_great_circle_center_rect \
+get_length \
+is_cross_line \
+is_in_latlon \
+is_inner \
+is_on_line \
+is_same_line \
+is_same_point \
+latlon2xyz \
+xyz2latlon \
+"
+
+
+for t in $TESTS; do \
+  exe=./test_$t;
+  ifile=input_$t.txt;
+  ofile=output_$t.txt;
+  echo -n "Do test for $t" ... ;
+  if  [ ! -x $exe ] ; then\
+    echo "Cannot execute $exe." && exit 1
+  else
+    $exe < $ifile > $ofile && echo "done."
+  fi
+  echo ""
+done
